7b162348d2a078e27e8c57ff94a27318ad748997
Dorna Raj Gyawali <dronarajgyawali@gmail.com>
2026-05-26 12:44:00 +0545

    graceful shutdown and server structured initilization
    Signed-off-by: Dorna Raj Gyawali <dronarajgyawali@gmail.com>

--- cmd/cache_proxy/main.go
+++ cmd/cache_proxy/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/drona-gyawali/cache-proxy/pkg/config"
 	proxyHTTP "github.com/drona-gyawali/cache-proxy/pkg/http"
@@ -19,14 +23,37 @@ func main() {
 
 	cfg := config.MustLoad()
 	proxyConfig := proxyHTTP.ProxyServerInit(cfg.CAPACITY, os.Getenv("PROXY_TOKEN"), cfg.ALLOWED_CLUSTERS)
-
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/proxy", proxyConfig.ProxyServer)
 
-	log.Printf("Cluster Booted %s", cfg.RUN_SERVER)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGTERM, syscall.SIGINT)
 
-	if err := http.ListenAndServe(cfg.RUN_SERVER, mux); err != nil {
-		log.Fatalf("Fatal system failure down inside HTTP network router: %v", err)
+	log.Printf("[SYSTEM] Proxy Server Up %s", cfg.RUN_SERVER)
+
+	server := http.Server {
+		Addr: cfg.RUN_SERVER,
+		Handler: mux,
 	}
+
+	go func () {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Fatal system failure down inside HTTP network router: %v", err)
+		}
+	}()
+	<- done
+
+	log.Printf("[SYSTEM] Sutting down the server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 5)
+	defer cancel()
+
+	_err := server.Shutdown(ctx)
+
+	if _err != nil {
+		log.Printf("[SYSTEM] Error Occured while closing the server %s", _err.Error())
+	}
+
+	log.Printf("[SYSTEM] Server closed")
+	
 }
